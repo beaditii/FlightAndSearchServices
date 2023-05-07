ca67c714243f3bff253fd328fc2f4bc08d046581
Aditi Kumari <aditisingh49893@gmail.com>
2023-05-07 19:13:40 +0530

    Completed city repository

--- src/repository/city_repository.js
+++ src/repository/city_repository.js
@@ -4,10 +4,11 @@ const { City }=require('../models/index');
 class CityRepository{
     async createCity({ name }){
         try{
-            const city=await City.create({name:name});
+            const city=await City.create({ name });
             return city;
         }
         catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -19,10 +20,39 @@ class CityRepository{
                     id:cityId
                 }
             });
+            return true;
         }
         catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
+    }
+
+    async updateCity(cityId,data){
+        try{
+    const city=await City.update(data,{
+        where:{
+            id:cityId
+        }
+    });
+    return city;
+        }
+
+        catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+
+    async getCity(cityId){
+try{
+const city=await City.findByPk(cityId);
+  return city;
+}
+catch(error){
+    console.log("Something went wrong in the repository layer");
+    throw{error};
+}
     }
 
 }
